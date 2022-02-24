3e078f8770743549b722382ec5d412a30b9fdcc5
Gavin Morrice <gavin@gavinmorrice.com>
2022-02-24 12:10:26 +0000

    Initial setup

---
+++ lib/jiminy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jiminy/version"
+
+module Jiminy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jiminy_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jiminy do
+  it "has a version number" do
+    expect(Jiminy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
