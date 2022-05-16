5217355b6b9b5f0d92e1ff0b3f2ecf4e7612b7bc
Gavin Morrice <gavin@gavinmorrice.com>
2022-05-16 15:42:00 +0100

    Move RSpec requirement into gem

--- lib/jiminy.rb
+++ lib/jiminy.rb
@@ -3,6 +3,7 @@
 require_relative "jiminy/version"
 require_relative "jiminy/setup"
 require_relative "jiminy/recording" if defined?(Rails)
+require_relative "jiminy/rspec" if ENV["RAILS_ENV"] == "test"
 
 module Jiminy
   module_function
