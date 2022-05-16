be2bc6c9a46d22e7353d5489fdbec252fdf7e44e
Gavin Morrice <gavin@gavinmorrice.com>
2022-05-16 15:56:22 +0100

    Replace logger debug with blocks

--- lib/jiminy/recording/prosopite_ext/tmp_file_recorder.rb
+++ lib/jiminy/recording/prosopite_ext/tmp_file_recorder.rb
@@ -6,24 +6,26 @@ module Jiminy
       class TmpFileRecorder
         require_relative "../n_plus_one"
 
+        # rubocop:disable Metrics/AbcSize
         def record(location:, queries:)
           yaml_content = File.read(Jiminy.config.temp_file_location)
           array = YAML.safe_load(yaml_content)
           n_plus_one = NPlusOne.new(location: location, queries: queries)
 
           if filepath_ignored?(n_plus_one.file)
-            Jiminy.logger.debug("Ignoring n+1 instance #{n_plus_one}")
+            Jiminy.logger.debug { "Ignoring n+1 instance #{n_plus_one}" }
             return
           end
 
           if location_in_array?(location, array)
-            Jiminy.logger.debug("Already reported n+1 instance #{n_plus_one}")
+            Jiminy.logger.debug { "Already reported n+1 instance #{n_plus_one}" }
             return
           end
 
           array << n_plus_one.to_h
           File.write(Jiminy.config.temp_file_location, array.to_yaml)
         end
+        # rubocop:enable Metrics/AbcSize
 
         private
 
