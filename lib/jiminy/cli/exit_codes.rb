4214115a2b0ec09ceea30d6386e500dcaba47ead
Gavin Morrice <gavin@gavinmorrice.com>
2022-05-24 10:58:02 +0100

    Update lib/jiminy/cli/exit_codes.rb
    Co-authored-by: Prem Sichanugrist <s@sikac.hu>

--- lib/jiminy/cli/exit_codes.rb
+++ lib/jiminy/cli/exit_codes.rb
@@ -4,10 +4,3 @@ require_relative "exit_codes/process_timeout"
 require_relative "exit_codes/success"
 require_relative "exit_codes/workflow_not_found"
 require_relative "exit_codes/workflow_not_success"
-
-module Jiminy
-  class CLI
-    module ExitCodes
-    end
-  end
-end
