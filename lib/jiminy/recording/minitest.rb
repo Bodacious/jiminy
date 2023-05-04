1bbe3f9ecd39ae3f9dddbdc4c6a9e7436afad204
Tom Winter <tom.winter@meetcleo.com>
2023-05-04 17:26:19 +0100

    Extend Minitest to setup Jiminy

---
+++ lib/jiminy/recording/minitest.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Jiminy
+  module Recording
+    require "jiminy/recording/prosopite_ext/send_notifications_with_tmp_file"
+
+    module Minitest
+      require "prosopite"
+      require "jiminy/recording/test_controller_concern"
+
+      ::Prosopite.singleton_class.prepend ProsopiteExt::SendNotificationsWithTmpFile
+
+      def before_setup
+        super
+        Prosopite.tmp_file = true
+        ActionController::Base.include(Jiminy::Recording::TestControllerConcern)
+      end
+
+      def after_teardown
+        super
+        Prosopite.tmp_file = false
+      end
+
+      Jiminy.extend(self)
+    end
+  end
+end
+
+class MiniTest::Test
+  include Jiminy::Recording::Minitest
+end
